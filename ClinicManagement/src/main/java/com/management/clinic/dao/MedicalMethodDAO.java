79a882698c6644fd8f50a5a11f9fa97ed94cfcc6
doubleth <96966537+HoangLV12@users.noreply.github.com>
2022-02-25 16:21:41 +0700

    complete Medical method module

--- ClinicManagement/src/main/java/com/management/clinic/dao/MedicalMethodDAO.java
+++ ClinicManagement/src/main/java/com/management/clinic/dao/MedicalMethodDAO.java
@@ -1,4 +1,20 @@
 package com.management.clinic.dao;
 
+import com.management.clinic.entity.MedicalMethod;
+import com.management.clinic.entity.MedicalResult;
+import java.util.List;
+
 public interface MedicalMethodDAO {
+
+  Long save(MedicalMethod medicalMethod);
+
+  MedicalMethod update(MedicalMethod medicalMethod);
+
+  MedicalMethod findById(Long id);
+
+  boolean delete(Long id);
+
+  List<MedicalResult> findAll();
+
+  List<MedicalMethod> findByResultId(Long resultId);
 }
--- ClinicManagement/src/main/java/com/management/clinic/service/impl/MedicalMethodServiceImpl.java
+++ ClinicManagement/src/main/java/com/management/clinic/service/impl/MedicalMethodServiceImpl.java
@@ -1,39 +1,65 @@
 package com.management.clinic.service.impl;
 
+import com.management.clinic.dao.MedicalMethodDAO;
 import com.management.clinic.entity.MedicalMethod;
 import com.management.clinic.service.MedicalMethodService;
+import java.util.Date;
 import java.util.List;
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
 public class MedicalMethodServiceImpl implements MedicalMethodService {
 
+  @Inject
+  private MedicalMethodDAO medicalMethodDAO;
+
   @Override
   public MedicalMethod save(MedicalMethod medicalMethod) {
+    Long id = medicalMethodDAO.save(medicalMethod);
+    if (id != null) {
+      return this.findById(id);
+    }
     return null;
   }
 
   @Override
   public MedicalMethod update(MedicalMethod medicalMethod) {
-    return null;
+    return medicalMethodDAO.update(medicalMethod);
   }
 
   @Override
   public MedicalMethod findById(Long id) {
-    return null;
+    return medicalMethodDAO.findById(id);
   }
 
   @Override
   public boolean delete(Long id) {
+    try {
+      return medicalMethodDAO.delete(id);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     return false;
   }
 
   @Override
   public List<MedicalMethod> findByResultId(Long resultId) {
-    return null;
+    return medicalMethodDAO.findByResultId(resultId);
   }
 
   @Override
   public MedicalMethod buildData(HttpServletRequest req) {
+    if (req != null) {
+      MedicalMethod.builder()
+          .id(Long.parseLong(req.getParameter("id")))
+          .name(req.getParameter("name"))
+          .diagnosis(req.getParameter("diagnosis"))
+          .conclude(req.getParameter("conclude"))
+          .createdStamp(new Date(req.getParameter("id")))
+          .modifiedStamp(new Date(req.getParameter("modifiedStamp")))
+          .resultId(Long.parseLong(req.getParameter("resultId")))
+          .build();
+    }
     return null;
   }
 }
